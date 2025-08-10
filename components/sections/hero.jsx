0a592e90f421044c18aa499add2e3c44c7603795
JuniorDCoder <foryoungjuniorngu@gmail.com>
2025-08-10 22:56:41 +0100

    fix: Ensure hero section takes up full vertical height of the screen

--- components/sections/hero.jsx
+++ components/sections/hero.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Button from "@/components/button";
 
 const Hero = (props) => (
-    <section className="bg-primary min-h-[600px] relative flex flex-col items-center justify-start py-6">
+    <section className="bg-primary min-h-screen relative flex flex-col items-center justify-start py-6">
         <div data-aos="fade-up" className="flex flex-col gap-4 items-center justify-center text-white text-center z-20 pt-8">
             <Image
                 src="/banner.svg"
